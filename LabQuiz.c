20f7037642afac32f5664f3e897e0a43ba5ad4eb
Hsundare <117363607+Hsundare@users.noreply.github.com>
2022-11-03 09:36:00 -0400

    Update LabQuiz.c

--- LabQuiz.c
+++ LabQuiz.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+int main()
+{
+      int num,factorial=0,ii;
+
+    printf("Enter a positive int: ");
+    scanf("%d",&num);
+    
+    for(ii=1;ii<=num;ii++) 
+    {
+        factorial += ii;
+    }
+    
+    printf("the factorial is %d",factorial);
+    
+
+    return 0;
 }
